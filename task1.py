ca8e13b549470172d0bc49de88a0b59f4a4bc88f
Anders Stiksrud Helmen <anders.s.helmen@gmail.com>
2021-12-02 13:09:32 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -11,11 +11,22 @@ def inputfile_to_array(input_file):
 
 number_list = [int(item) for item in inputfile_to_array("input_1")]
 
-num_increases = 0
-prev_num = 999999
-for i in number_list:
-  if i > prev_num:
-    num_increases += 1
-  prev_num = i
-print(num_increases)
+def calculate_num_increases(number_list):
+  num_increases = 0
+  prev_num = 9999999999999
+  for element in number_list:
+    if element > prev_num:
+      num_increases += 1
+    prev_num = element
+  return num_increases
   
+def calculate_sliding_window(number_list):
+  i = 0
+  sliding_window_list = []
+  for idx, val in enumerate(number_list):
+    if idx < (len(number_list) - 2):
+      window_sum = val + number_list[idx +1] + number_list[idx +2]
+      sliding_window_list.append(window_sum)
+  return sliding_window_list
+
+print (calculate_num_increases(calculate_sliding_window(number_list)))
