7a234d8d7565c7a86986e428aed93db701c8b900
Anders Stiksrud Helmen <anders.s.helmen@gmail.com>
2021-12-02 14:53:22 +0100

    Update task2.py

--- task2.py
+++ task2.py
@@ -27,6 +27,25 @@ def get_pos(coordinate_list):
       print("Something is wrong")
   return (vert_pos,horiz_pos)
 
+def get_new_pos(coordinate_list):
+  vert_pos = 0
+  horiz_pos = 0
+  aim = 0
+  for element in coordinate_list:
+    dir = element.split()[0]
+    num = int(element.split()[1])
+    if dir == "forward":
+      horiz_pos += num
+      vert_pos += aim * num
+    elif dir == "up":
+      aim -= num
+    elif dir == "down":
+      aim += num
+    else:
+      print("Something is wrong")
+  return (vert_pos,horiz_pos,aim)
+  
 vert_pos, horizon_pos = get_pos(coordinate_list)
-print (vert_pos * horizon_pos)
-
+new_vert_pos, new_horizon_pos, aim = get_new_pos(coordinate_list)
+print ("First answer: ", vert_pos * horizon_pos)
+print ("Second answer: ", new_vert_pos * new_horizon_pos)
